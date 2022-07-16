dec052b291af88a52c2573c5d1a6a9c574132bdd
Nguyen Huu Giang <nguyenhuugiangtb08012001@gmail.com>
2022-07-16 23:02:27 +0700

    update file

--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@ const route = require('./routes');
 const app = express();
 const port = 3002;
 
-                app.use(express.static(path.join(__dirname, 'public')))
+          app.use(express.static(path.join(__dirname, 'public')))
 
 //HTTP logger
-                    app.use(morgan('combined'));
+app.use(morgan('combined'));
 
-app.engine('.hbs', engine({extname: '.hbs'}));
+app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
-app.set('views', path.join(__dirname,'resources/views'));
+app.set('views', path.join(__dirname, 'resources/views'));
 
 
 // Route init
