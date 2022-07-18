50dae301742b2727b677e4b27f44766aa3684db1
nguyenhuugiang08 <77537697+nguyenhuugiang08@users.noreply.github.com>
2022-07-18 20:55:11 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const app = express();
 const port = 8000;
 
 const corsOptions = {
-  origin: 'http://localhost:3000/',
+  origin: 'http://localhost:3000',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 app.use(cors(corsOptions));
